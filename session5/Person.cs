37b2d07ed88a576a46c0680b076fe892d220e48f
DamBinh1 <118426540+DamBinh1@users.noreply.github.com>
2023-08-16 22:59:54 +0700

    Create Person.cs

---
+++ session5/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laptrinhApc2.session5
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+
+        public Person(string name, string phone)
+        {
+            Name = name;
+            Phone = phone;
+        }
+    }
+}
